b5f2679bced5e5d66a4ab6bab4a7bb69cbb6f803
Angella Lao <6369@holbertonstudents.com>
2023-09-26 14:41:42 +1000

    add check api status function

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,4 +21,18 @@ $(document).ready(() => {
 
     updateAmenities();
   });
+  const checkAPIStatus = () => {
+    $.ajax({
+      type: 'GET',
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      success: (data) => {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+      }
+    });
+  };
+  checkAPIStatus();
 });
