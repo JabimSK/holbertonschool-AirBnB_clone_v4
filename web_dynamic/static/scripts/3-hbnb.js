788256ac820c6f89682182ee5162be6402b33ed3
Angella Lao <6369@holbertonstudents.com>
2023-09-26 16:23:59 +1000

    fetch places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,88 @@
+$(document).ready(() => {
+  const amenityIDs = {};
+
+  // Function to update the h4 tag with the list of amenities checked
+  const updateAmenities = () => {
+    const checkedAmenities = Object.values(amenityIDs);
+    const amenitiesText = checkedAmenities.join(', ');
+    $('.amenities h4').text(amenitiesText);
+  };
+
+  // Listen for changes on input checkboxes
+  $('.amenities ul').on('change', 'input[type="checkbox"]', function (e) {
+    const amenityID = $(this).data('id'); // Get the Amenity ID from the data-id attribute
+    const amenityName = $(this).data('name');
+
+    if ($(this).prop('checked')) {
+      amenityIDs[amenityID] = amenityName;
+    } else {
+      delete amenityIDs[amenityID];
+    }
+
+    updateAmenities();
+  });
+  const checkAPIStatus = () => {
+    $.ajax({
+      type: 'GET',
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      success: (data) => {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+      }
+    });
+  };
+  checkAPIStatus();
+
+  /*const fetchPlaces = () => {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: function (data) {
+        displayPlaces(data); // Call a function to display the places
+      },
+      error: function (error) {
+        console.error('Error searching places:', error);
+      }
+    });
+  };
+  fetchPlaces();*/
+
+  const searchPlaces = () => {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      contentType: 'application/json',
+      data: JSON.stringify({}), 
+      success: function (data) {
+        //console.log(data);
+        displayPlaces(data); 
+      },
+      error: function (error) {
+        console.error('Error searching places:', error);
+      }
+    });
+  };
+  
+  // Function to display places in the HTML
+  const displayPlaces = (places) => {
+    const placesSection = $('.places'); 
+    placesSection.empty(); 
+
+    places.forEach((place) => {
+      const article = $('<article>');
+      article.append(`<div class="title"><h2>${place.name}</h2></div>`);
+      article.append(`<div class="price_by_night">$${place.price_by_night} per night</div>`);
+      article.append(`<div class="information"><div class="max_guest">${place.max_guest} Guests</div><div class="number_rooms">${place.number_rooms} Bedrooms</div><div class="number_bathrooms">${place.number_bathrooms} Bathroom</div></div>`);
+      article.append(`<div class="description">${place.description}</div>`);
+  
+      placesSection.append(article);
+    });
+  };
+  searchPlaces();
+
+});
