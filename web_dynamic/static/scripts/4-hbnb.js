e380518903f643898c5efd560d8043dacb12a233
Angella Lao <6369@holbertonstudents.com>
2023-09-28 11:06:25 +1000

    conditional operator for plurals, reuse function

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,14 +1,12 @@
 $(document).ready(() => {
   const amenityIDs = {};
 
-  // Function to update the h4 tag with the list of amenities checked
   const updateAmenities = () => {
     const checkedAmenities = Object.values(amenityIDs);
     const amenitiesText = checkedAmenities.join(', ');
     $('.amenities h4').text(amenitiesText);
   };
 
-  // Listen for changes on input checkboxes
   $('.amenities ul').on('change', 'input[type="checkbox"]', function (e) {
     const amenityID = $(this).data('id'); // Get the Amenity ID from the data-id attribute
     const amenityName = $(this).data('name');
@@ -37,12 +35,12 @@ $(document).ready(() => {
   };
   checkAPIStatus();
 
-  const searchPlaces = () => {
+  const searchPlaces = (data = {}) => {
     $.ajax({
       type: 'POST',
       url: 'http://0.0.0.0:5001/api/v1/places_search',
       contentType: 'application/json',
-      data: JSON.stringify({}),
+      data: JSON.stringify(data),
       success: function (data) {
         console.log(data);
         displayPlaces(data);
@@ -59,30 +57,28 @@ $(document).ready(() => {
 
     for (const place of placesData) {
       const article = $('<article>');
-      article.append(`<div class="title_box"><h2>${place.name}</h2><div class="price_by_night">$${place.price_by_night} per night</div></div>`);
-      article.append(`<div class="information"><div class="max_guest">${place.max_guest} Guests</div><div class="number_rooms">${place.number_rooms} Bedrooms</div><div class="number_bathrooms">${place.number_bathrooms} Bathroom</div></div>`);
-      article.append(`<div class="description">${place.description}</div>`);
+      article.append(`
+      <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">$${place.price_by_night} per night</div>
+      </div>`);
+      article.append(`
+      <div class="information">
+        <div class="max_guest">${place.max_guest} ${place.max_guest !== 1 ? 'Guests' : 'Guest'}</div>
+        <div class="number_rooms">${place.number_rooms} ${place.number_rooms !== 1 ? 'Bedrooms' : 'Bedroom'}</div>
+        <div class="number_bathrooms">${place.number_bathrooms} ${place.number_bathrooms !== 1 ? 'Bathrooms' : 'Bathroom'}</div>
+      </div>`);
+      article.append(`
+      <div class="description">${place.description}</div>`);
 
       placesSection.append(article);
     }
   };
 
   $('button').on('click', function () {
-    // Make a new POST request to places_search with the list of checked Amenities
-    $.ajax({
-      type: 'POST',
-      url: 'http://0.0.0.0:5001/api/v1/places_search',
-      contentType: 'application/json',
-      data: JSON.stringify({
-        amenities: Object.keys(amenityIDs), // Pass the list of checked amenity IDs
-      }),
-      success: function (data) {
-        displayPlaces(data);
-      },
-      error: function (error) {
-        console.error('Error searching places:', error);
-      }
-    });
+    // Make a new POST request to places_search, passing the list of checked Amenities
+    const checkedAmenitiesList = Object.keys(amenityIDs);
+    searchPlaces({'amenities': checkedAmenitiesList});
   });
 
   searchPlaces();
